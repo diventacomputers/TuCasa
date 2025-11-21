52469a737e0ab935383104d8c5fedd27c5ba8de7
Laptop Crepes tecl90 <Diventacomputers@gmail.com>
2025-11-21 12:25:05 -0500

    npm run build error fixed global.d.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("No se encontró el elemento root");
+}
+
+createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
